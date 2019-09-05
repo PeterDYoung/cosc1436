169ca7fc478a0dcebe6a9518f1edf780d121d10b
Peter <peter.young068@my.tccd.edu>
2019-09-05 12:49:02 -0500

    initial

---
+++ Projects/InClass/cosc1438.AccountBalance/Classwork1/Classwork1.cpp
@@ -0,0 +1,33 @@
+// Classwork1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{	
+	double balance, deposits, withdrawal, interest;
+    cout << "Enter ballance:";
+	cin >> balance;
+	cout << "\nEnter deposits:";
+	cin >> deposits;
+	cout << "\nEnter withdrawal:";
+	cin >> withdrawal;
+	cout << "\nEnter interest:";
+	cin >> interest;
+
+	cout <<"\nCurrent balance: "<< interest * balance + balance + deposits - withdrawal;
+
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
